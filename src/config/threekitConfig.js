df92a27781dc791559934421cece337ea863a132
joel1001 <40803308+joel1001@users.noreply.github.com>
2021-12-06 22:26:23 -0600

    update auth

--- src/config/threekitConfig.js
+++ src/config/threekitConfig.js
@@ -1,6 +1,6 @@
 export default {
     // *********local***********
-    authToken: window.location.hostname.includes("localhost") ? 'ce1879a4-4f3d-4e6c-a3ed-eed91fe9cdb3' : "170012e5-6089-46a7-8151-02b712535a48",
+    authToken: window.location.hostname.includes("localhost") ? 'ce1879a4-4f3d-4e6c-a3ed-eed91fe9cdb3' : "0b52e3b4-f6dd-455b-8b7d-e866bb3dd2b3",
     assetId: '5f1fea7e-4ba1-4e8c-9d75-f3bac22e6779',
     orgId: 'dbb6f333-2a61-4412-946a-5ddeb4228e28',
     threekitScriptURL: 'https://preview.threekit.com/app/js/threekit-player.js',
